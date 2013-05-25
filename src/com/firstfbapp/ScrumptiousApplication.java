370011dd415e9a4780568985281ce7f59c24504a
chameleon <chameleon.rising@gmail.com>
2013-05-25 09:04:19 +0700

    Friend Picker - Not Finished Yet

---
+++ src/com/firstfbapp/ScrumptiousApplication.java
@@ -0,0 +1,18 @@
+package com.firstfbapp;
+
+import java.util.List;
+
+import android.app.Application;
+
+import com.facebook.model.GraphUser;
+
+public class ScrumptiousApplication extends Application {
+	private List<GraphUser> selectedUsers;
+	public List<GraphUser> getSelectedUsers() {
+	    return selectedUsers;
+	}
+
+	public void setSelectedUsers(List<GraphUser> users) {
+	    selectedUsers = users;
+	}
+}
